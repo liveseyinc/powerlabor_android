f1918055d49fce3f8cb5915063a80e0fb389a297
powerlabor <powerlabor@gmail.com>
2012-07-23 18:23:29 +0400

    Create android project

---
+++ src/org/powerlabor/main/Powerlabor_androidActivity.java
@@ -0,0 +1,13 @@
+package org.powerlabor.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Powerlabor_androidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
